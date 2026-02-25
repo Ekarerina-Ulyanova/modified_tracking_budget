9d207896af61e9cc34e80da5a053733e6d6d4279
Ekarerina-Ulyanova <0ulyanova.ekaterina.4@gmail.com>
2026-02-25 18:21:07 +0300

    osa_tool recommendations

---
+++ modified_tracking_budget/__init__.py
@@ -0,0 +1,6 @@
+# Package initializer for modified_tracking_budget
+__version__ = "0.1.0"
+
+# Export commonly used components here if desired
+# from .budget_manager import BudgetManager
+# from .database import Database
